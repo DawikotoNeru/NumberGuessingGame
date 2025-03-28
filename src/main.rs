7d1fe24628d92a5452dbd563db1c271ca1a38a1b
DawikotoNeru <cozyandstudychannel@gmail.com>
2025-03-27 22:07:00 -0300

    inserting the loop that checks the moves handling possible errors in inserting letters or numbers outside the scope. inserting messages for each situation creating a function that checks if the number is greater or less than the secret number creating variables to receive user errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use rand;
 use std::io::{stdin, stdout, Write};
+use rand::Rng;
 
 enum Difficulties{
     Easy,
@@ -9,60 +9,92 @@ enum Difficulties{
 }
 
 fn welcome_message(){
-    println!("Welcome to the number guessing game\n
-    \n
-    The game will have 3 difficulties, which will influence the \n
-    number of chances you have to guess the secret number");
+    println!("== Welcome to the number guessing game ==");
+    print!("\n");
+    println!("- The game will have 3 difficulties, which will influence the");
+    print!("\n");
+    println!("- number of chances you have to guess the secret number");
 }
 
 fn options_message(){
     println!("(1) - Easy\n(2) - Medium\n(3) - Hard\n(4) - VeryHard");
-    println!("What is the desired difficulty?")
+    println!("- Obs: If you choose an option outside the scope, the 'easy' difficulty will be the default.");
+    print!("\n");
+    println!("- What is the desired difficulty?")
+}
+
+fn analyze_guess(player_guess: usize, secret_num: usize){
+    if player_guess > secret_num {
+        println!("Your number is greater than the secret number");
+    } else {
+        println!("Your number is smaller than the secret number");
+    }
 }
 
 fn main() {
 
+    let mut maximum_guess: u32 = 12;
     let mut rng = rand::rng();
+    let secret_num = rng.random_range(1..=100);
     let difficulties = vec!(Difficulties::Easy, Difficulties::Medium, Difficulties::Hard, Difficulties::VeryHard);
-
+    let mut player_error_quantity: u32 = 0;
 
     welcome_message();
     options_message();
 
     print!("> ");
-    stdout().flush().err();
+    stdout().flush().unwrap();
 
     let mut player_choosing: String = String::new();
     stdin()
         .read_line(&mut player_choosing)
         .expect("input error");
 
-    let player_choosing = player_choosing.trim().parse::<usize>().unwrap_or_else(|_| 0);
+    let player_choosing: usize = player_choosing.trim().parse::<usize>().unwrap_or_else(|_| 1);
 
 
-    match difficulties.get(player_choosing-1){
-        Some(Difficulties::Easy) => {
-            const MAXIMUM_GUESS: u32 = 12;
-            println!("PRINT EASY");
+    if player_choosing > 0 && player_choosing <= difficulties.len() {
+        match difficulties.get(player_choosing - 1) {
+            Some(Difficulties::Easy) => { maximum_guess = 12; }
+            Some(Difficulties::Medium) => { maximum_guess = 8; }
+            Some(Difficulties::Hard) => { maximum_guess = 5; }
+            Some(Difficulties::VeryHard) => { maximum_guess = 3; }
+            _ => {
+                println!("Erro")
+            }
         }
+    } else {
+        maximum_guess = 12;
+        println!("Invalid option. Easy difficulty set as default")
+    }
 
-        Some(Difficulties::Medium) => {
-            const MAXIMUM_GUESS: u32 = 8;
-            println!("PRINT MEDIUM");
-        }
+    loop {
+        if player_error_quantity <= maximum_guess {
+            print!("Enter your guess: ");
+            stdout().flush().unwrap();
 
-        Some(Difficulties::Hard) => {
-            const MAXIMUM_GUESS: u32 = 5;
-            println!("PRINT HARD");
-        }
+            let mut player_guess: String = String::new();
+            stdin()
+                .read_line(&mut player_guess)
+                .expect("Error writing your guess");
 
-        Some(Difficulties::VeryHard) => {
-            const MAXIMUM_GUESS: u32 = 3;
-            println!("PRINT VERY HARD");
-        }
+            match player_guess.trim().parse() {
+                Ok(num) => {
+                    if num == secret_num{
+                        println!("Winner!!");
+                        println!("\nAttempts: {}", player_error_quantity);
+                        break;
+                    } else {
+                        analyze_guess(num, secret_num);
+                        player_error_quantity += 1;
+                    }
+                }
+                Err(_) => println!("Insert only positive numbers")
+            }
 
-        _ => {
-            println!("This option does not exist")
+        } else {
+            println!("Game over, Thanks for playing.");
+            break;
         }
     }
 
